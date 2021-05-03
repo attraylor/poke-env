0cbf6bc73c4c873cd5a922e93c373573f55bcdef
Aaron <attraylor@gmail.com>
2021-05-03 11:08:43 -0400

    reverted stable idx, hopefully

--- examples/BigBoy/players.py
+++ examples/BigBoy/players.py
@@ -45,32 +45,12 @@ class BigBoyRLPlayer(Gen8EnvSinglePlayer):
 	def embed_battle(self, battle):
 		final_state_dict = {}
 		team = []
-<<<<<<< HEAD
-		for idx in range(0, 6):
-			pokemon_name = self.stable_team_inv[idx]
-			pokemon_obj = battle.team[pokemon_name]
-			if pokemon_obj.fainted == True:
-				team.append(None)
-			else:
-				team.append(pokemon_obj)
-
-		#active_pokemon = battle.active_pokemon
-		#switches = battle.available_switches
-		#fainted_padding = [None] * (5 - len(switches))
-		pokemon_objects = [active_pokemon] + team
-		assert len(pokemon_objects) == 6 + 1
-		team_order = ["active", 0, 1, 2, 3, 4, 5]
-		#for idx, pokemon in enumerate(pokemon_objects):#battle.team.keys(): #thought this was a method
-
-		for idx, pokemon in zip(team_order, pokemon_objects):
-=======
 		active_pokemon = battle.active_pokemon
 		switches = battle.available_switches
 		fainted_padding = [None] * (5 - len(switches))
 		pokemon_objects = [active_pokemon] + switches + fainted_padding
 		assert len(pokemon_objects) == 6
 		for idx, pokemon in enumerate(pokemon_objects):#battle.team.keys(): #thought this was a method
->>>>>>> parent of 200eb8b... First stab at stable team indexing
 			#pokemon = battle.team[pokemon_key]
 			pokemon_information = {}
 
